cd9e2efc442c0ba06f6c5c00e969f2db253c90ab
Tortosa <sofianetortosa@gmail.com>
2023-12-14 16:26:56 +0100

    création HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController
 {
+    #[Route('/', 'home.index', methods: ['GET'])]
     public function index(): Response
     {
-        
+        return $this->render('home.html.twig');
     }
 }
